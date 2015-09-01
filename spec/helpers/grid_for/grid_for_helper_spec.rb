971eca7687d639fa05cdcfe3d021b8ed724f47bd
Ole J. Rosendahl <ole.johnny.rosendahl@gmail.com>
2015-09-01 17:04:28 +0700

    Extract helper builder in specs

--- spec/helpers/grid_for/grid_for_helper_spec.rb
+++ spec/helpers/grid_for/grid_for_helper_spec.rb
@@ -2,12 +2,15 @@ require "spec_helper"
 
 describe GridFor::GridForHelper do
   describe GridFor::GridForHelper::GridForBuilder do
-
     let(:object) { Struct.new(:name).new("Name") }
+
+    def builder
+      @builder ||= described_class.new(object, ActionView::Base.new)
+    end
 
     describe "#row(options, &block)" do
       it "renders a div with bootstrap row css" do
-        expect(described_class.new(object, ActionView::Base.new).row { "Hello" }).to eq(
+        expect(builder.row { "Hello" }).to eq(
           "<div class=\"row-fluid\">Hello</div>"
         )
       end
@@ -15,13 +18,13 @@ describe GridFor::GridForHelper do
 
     describe "#col(options = {}, &block)" do
       it "renders a span of the given size with the size option" do
-        expect(described_class.new(object, ActionView::Base.new).col(size: 4) { "Hello" }).to eq(
+        expect(builder.col(size: 4) { "Hello" }).to eq(
           "<div class=\"span4\">Hello</div>"
         )
       end
 
       it "renders a div spanning 12 by default" do
-        expect(described_class.new(object, ActionView::Base.new).col { "Hello" }).to match(
+        expect(builder.col { "Hello" }).to match(
           %r"class=\"span12\""
         )
       end
